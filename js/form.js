896131cf26bf2fc33f84bd7d7e48e174097d3b53
uzomaker <uzochukwuemeka516@gmail.com>
2022-09-28 23:17:02 +0100

    fellowship

---
+++ js/form.js
@@ -0,0 +1,44 @@
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress-line");
+const formStep = document.querySelectorAll(".form-step");
+const pogressSteps = document.querySelectorAll(".progress-step ");
+
+let formStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum++;
+    updateFormSteps();
+    updateProgessbar();
+  });
+});
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum--;
+    updateFormSteps();
+    updateProgessbar();
+  });
+});
+
+function updateFormSteps() {
+  formStep.forEach((formstep) => {
+    formstep.classList.contains("formstep-active") &&
+      formstep.classList.remove("formstep-active");
+  });
+  formStep[formStepsNum].classList.add("formstep-active");
+}
+
+function updateProgessbar() {
+  pogressSteps.forEach((pogressStep, idx) => {
+    if (idx < formStepsNum + 1) {
+      pogressStep.classList.add("progress-step-active");
+    } else {
+      pogressStep.classList.remove("progress-step-active");
+    }
+    const progressActive = document.querySelectorAll(".progress-step-active");
+
+    progress.style.width =
+      ((progressActive.length - 1) / (pogressSteps.length - 1)) * 100 + "%";
+  });
+}
